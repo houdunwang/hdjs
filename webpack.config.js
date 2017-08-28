11acc39ee8021550b00d5e68a4857f223baa436d
houdunwang <2300071698@qq.com>
2017-08-28 13:25:13 +0800

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const webpack = require('webpack');
-
 function resolve(dir) {
     return path.join(__dirname, '..', dir)
 }
-
 module.exports = {
     entry: {app: './src/app.js', hdjs: './src/hdjs.js'},
     output: {
@@ -23,7 +21,7 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js', '.vue', '.json'],
-        alias: {'vue$': 'vue/dist/vue.esm.js', '@': resolve('src')}
+        alias: {'vue': 'vue/dist/vue.js', '@': resolve('src')}
     },
     //热加载使用的项目目录
     devServer: {contentBase: path.join(__dirname, "dist"), port: 9000},
@@ -35,7 +33,7 @@ module.exports = {
         //生成css文件
         new ExtractTextPlugin("hdjs.css"),
         //模板文件并生成到dist目录中，用于热加载使用
-        new HtmlWebpackPlugin({title: 'hdjs-vue',  template: __dirname + '/src/module.html'}),
+        new HtmlWebpackPlugin({title: 'hdjs-vue', template: __dirname + '/src/module.html'}),
         new webpack.ProvidePlugin({$: "jquery", jQuery: "jquery", "window.jQuery": "jquery"}),
         new CopyWebpackPlugin([{from: 'static', to: 'static'}])
     ],
