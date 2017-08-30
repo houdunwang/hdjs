3135b6be1094c29785fba6ebdb0424532022680b
houdunwang <2300071698@qq.com>
2017-08-30 15:37:11 +0800

    no message

--- dist/static/requirejs/config.js
+++ dist/static/requirejs/config.js
@@ -5,16 +5,8 @@
 if (!window.hdjs) {
     window.hdjs = {node_modules: '', base: '', uploader: '', filesLists: ''};
 }
-//兼容旧版本，用于处理旧版本只配置的hdjs.base的情况
-if (window.hdjs.base && !window.hdjs.node_modules) {
-    var pos = window.hdjs.base.lastIndexOf('/');
-    window.hdjs.node_modules = window.hdjs.base.substr(0, pos) + '/node_modules';
-}
-if (!window.hdjs.node_modules) {
-    window.hdjs.node_modules = '/node_modules';
-}
 if (!window.hdjs.base) {
-    window.hdjs.base = window.hdjs.node_modules + '/hdjs';
+    window.hdjs.base =  '/node_modules/hdjs';
 }
 require.config({
     baseUrl: window.hdjs.base,
