3008b6bce02271204c860ce73ff44d58f8aabca3
houdunwang <2300071698@qq.com>
2018-04-01 22:20:01 +0800

    优化

--- dist/static/requirejs/config.js
+++ dist/static/requirejs/config.js
@@ -7,7 +7,7 @@
 //     node_modules: '', base: '/node_modules/hdjs/', uploader: '', filesLists: '', hdjs: ''
 // }, window.hdjs);
 require.config({
-    urlArgs: 'version=1.2.102',
+    urlArgs: 'version=1.2.105',
     baseUrl: window.hdjs.base,
     paths: {
         hdjs: 'dist/static/hdjs',
